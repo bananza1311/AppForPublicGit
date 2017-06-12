cf9ac281eb96f97a7891c32615d79fc99ccf70f1
Александр Муравьев <aleksandrmuravev@MacBook-Pro-Aleksandr.local>
2017-06-13 00:54:47 +0300

    Initial Commit

---
+++ AppForPublicGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppForPublicGit
+//
+//  Created by Александр Муравьев on 13.06.17.
+//  Copyright © 2017 Александр Муравьев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
